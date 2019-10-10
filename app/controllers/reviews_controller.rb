cc1b3d0e52a013b58f8de0c1815496e97f468253
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-10 23:44:58 +0800

    Figured out how to return empty array JSON to frontend if the podcast doesn't yet exist and therefore doesn't have corresponding reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,19 @@
 class ReviewsController < ApplicationController
 
   def create
-    # raise params.inspect
-    user=User.find_by(username: params[:username])
+    raise params.inspect
+    user=User.find_by(username: params[:username]) #user should have been created when the user logs in 
     podcast=Podcast.find_by(podcast_id: params[:podcast_id])
     review=Review.create(user: user, podcast:podcast, text:params[:review])
   end
 
   def index
     podcast=Podcast.find_by(podcast_id: params[:podcast_id])
-    if !!podcast
+    if !!podcast #if the podcast exists
       reviews=podcast.reviews
       render json: ReviewSerializer.new(reviews).to_serialized_json
     else
-      render json: {reviews:[]}
+      render json: [].to_json
     end
   end
 
