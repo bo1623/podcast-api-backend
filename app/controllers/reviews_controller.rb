dd6f0b6feb16b7d02d324ced8e6d3408544b46c4
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-10 14:41:30 +0800

    Completed create action within reviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,11 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise params.inspect
+    # raise params.inspect
+    byebug
+    user=User.find_by(username: params[:username])
+    podcast=Podcast.find_by(podcast_id: params[:podcast_id])
+    review=Review.create(user: user, podcast:podcast, text:params[:review])
   end
 
 end
